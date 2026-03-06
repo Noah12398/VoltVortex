ed43dab0a4af94cb24891b0c0fcefaddd073abc8
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2026-03-06 23:26:12 +0530

    Console Logging

--- Backend/app.py
+++ Backend/app.py
@@ -1,25 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from supabase_client import supabase
 
 app = Flask(__name__)
 CORS(app)
 
-try:
-    response = supabase.table("sensor_data").select("*").limit(1).execute()
-    print("Connected to Supabase!")
-    print(response.data)
+@app.route("/", methods=["POST"])
+def sensor_data():
+    data = request.json
 
-except Exception as e:
-    print("Connection failed:", e)
-    
-@app.route("/")
-def home():
-    return jsonify({"message": "Krishi-Net Backend Running"})
-
-@app.route("/soil")
-def soil():
-    return jsonify({"ph":6.5, "recommendation":"Balanced NPK fertilizer"})
+    print("Received sensor data:")
+    print(data)
+ 
+    return jsonify({"status": "data received"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
