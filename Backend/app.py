d94838c120c6f6bd509a8754f3ba3c089cfa30ab
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2026-03-06 22:05:02 +0530

    Database Connected

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from supabase_client import supabase
 
 app = Flask(__name__)
 CORS(app)
 
+try:
+    response = supabase.table("sensor_data").select("*").limit(1).execute()
+    print("Connected to Supabase!")
+    print(response.data)
+
+except Exception as e:
+    print("Connection failed:", e)
+    
 @app.route("/")
 def home():
     return jsonify({"message": "Krishi-Net Backend Running"})
